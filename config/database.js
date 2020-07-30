acf102a26098648f095889360df7e1736a9ef08b
srinathmerugu <62550907+srinathmerugu@users.noreply.github.com>
2020-07-30 17:37:21 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,3 @@
 module.exports = {
-    database : "mongodb+srv://srinathmerugu:vishal777@cluster0-6xkr9.mongodb.net/myCart?authSource=admin&replicaSet=Cluster0-shard-0&w=majority&readPreference=primary&retryWrites=true&ssl=true"
+    database : process.env.DATABASE;
 }
